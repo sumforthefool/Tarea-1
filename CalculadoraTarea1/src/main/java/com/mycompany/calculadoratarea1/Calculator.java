9e68b1a809a5578a433f638d3bc9c73a658750fe
ThonyV.N <91163138+sumforthefool@users.noreply.github.com>
2024-03-06 21:28:26 -0600

    delate dependencys and add logic
    Dalate innecesaries dependencys and add logic
    
    Co-Authored-By: Juan Pablo Moreno G <juanpmg2@gmail.com>

--- CalculadoraTarea1/src/main/java/com/mycompany/calculadoratarea1/Calculator.java
+++ CalculadoraTarea1/src/main/java/com/mycompany/calculadoratarea1/Calculator.java
@@ -9,5 +9,30 @@ package com.mycompany.calculadoratarea1;
  * @author Usuario
  */
 public class Calculator {
-    
+
+    // Método para sumar dos números enteros
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Método para restar el segundo número del primero
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Método para multiplicar dos números enteros
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Método para dividir el primer número por el segundo número
+    // Se verifica si el segundo número es cero para evitar una división por cero
+    // En caso de división por cero, se lanza una excepción IllegalArgumentException
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
+
