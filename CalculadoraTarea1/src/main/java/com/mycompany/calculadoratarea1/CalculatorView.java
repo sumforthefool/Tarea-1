842c05ec73931e809ba172f601c91eea0a6f5536
ThonyV.N <91163138+sumforthefool@users.noreply.github.com>
2024-03-06 21:42:41 -0600

    first test
    test to search a badwork

--- CalculadoraTarea1/src/main/java/com/mycompany/calculadoratarea1/CalculatorView.java
+++ CalculadoraTarea1/src/main/java/com/mycompany/calculadoratarea1/CalculatorView.java
@@ -4,10 +4,20 @@
  */
 package com.mycompany.calculadoratarea1;
 
+import java.awt.TextField;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Usuario
  */
 public class CalculatorView {
+    private TextField display; // Campo de texto para mostrar los números y resultados
+    private String currentInput = ""; // Almacena la entrada actual del usuario
     
+     @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Calculator"); // Título de la ventana
+        
+    }
 }
